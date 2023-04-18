4e3f687759cc912b0707a63f07b07f434a556214
Guy Vider <guy@guyvider.com>
2023-04-18 09:13:10 -0700

    refactor routes

--- main.go
+++ main.go
@@ -2,7 +2,8 @@ package main
 
 import (
 	"errors"
-	"net/http"
+	"log"
+	"os"
 
 	"github.com/gin-gonic/gin"
 )
@@ -18,74 +19,15 @@ func bookById(id string) (*book, error) {
 	return nil, errors.New("book with id " + id + " not found")
 }
 
-func getBooks(c *gin.Context) {
-	c.IndentedJSON(http.StatusOK, books)
-}
-
-func getBookByID(c *gin.Context) {
-	id := c.Param("id")
-	book, err := bookById(id)
-	if err != nil {
-		c.IndentedJSON(http.StatusNotFound, gin.H{"message": err.Error()})
-		return
-	}
-	c.IndentedJSON(http.StatusOK, book)
-}
-
-func checkoutBook(c *gin.Context) {
-	id, ok := c.GetQuery("id")
-	if !ok {
-		c.IndentedJSON(http.StatusBadRequest, gin.H{"message": "id parameter missing"})
-		return
-	}
-	book, err := bookById(id)
-	if err != nil {
-		c.IndentedJSON(http.StatusNotFound, gin.H{"message": err.Error()})
-		return
-	}
-	if book.Quantity == 0 {
-		c.IndentedJSON(http.StatusBadRequest, gin.H{"message": "no more copies left of book " + id})
-		return
-	}
-	book.Quantity -= 1
-	c.IndentedJSON(http.StatusOK, book)
-}
-
-func returnBook(c *gin.Context) {
-	id, ok := c.GetQuery("id")
-	if !ok {
-		c.IndentedJSON(http.StatusBadRequest, gin.H{"message": "id parameter missing"})
-		return
-	}
-	book, err := bookById(id)
-	if err != nil {
-		c.IndentedJSON(http.StatusNotFound, gin.H{"message": err.Error()})
-		return
-	}
-	book.Quantity += 1
-	c.IndentedJSON(http.StatusOK, book)
-}
-
-func createBook(c *gin.Context) {
-	var newBook book
-	if err := c.BindJSON(&newBook); err != nil {
-		return
-	}
-	if b, _ := bookById(newBook.ID); b != nil {
-		c.IndentedJSON(http.StatusConflict, gin.H{"message": "book with id " + newBook.ID + " already exists"})
-		return
-	}
-	books = append(books, newBook)
-	c.IndentedJSON(http.StatusCreated, newBook)
-}
-
 func main() {
-	loadBooks()
+	//load list of books to memory
+	if result, err := loadBooks(); err == nil {
+		books = append(books, result...)
+	} else {
+		log.Fatal(err)
+		os.Exit(-1)
+	}
 	router := gin.Default()
-	router.GET("/books", getBooks)
-	router.GET("/books/:id", getBookByID)
-	router.POST("/books", createBook)
-	router.PATCH("/books/checkout", checkoutBook)
-	router.PATCH("/books/return", returnBook)
+	setRoutes(router)
 	router.Run("localhost:8080")
 }
---
+++ routes.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func getBooks(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, books)
+}
+
+func getBookByID(c *gin.Context) {
+	id := c.Param("id")
+	book, err := bookById(id)
+	if err != nil {
+		c.IndentedJSON(http.StatusNotFound, gin.H{"message": err.Error()})
+		return
+	}
+	c.IndentedJSON(http.StatusOK, book)
+}
+
+func checkoutBook(c *gin.Context) {
+	id, ok := c.GetQuery("id")
+	if !ok {
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"message": "id parameter missing"})
+		return
+	}
+	book, err := bookById(id)
+	if err != nil {
+		c.IndentedJSON(http.StatusNotFound, gin.H{"message": err.Error()})
+		return
+	}
+	if book.Quantity == 0 {
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"message": "no more copies left of book " + id})
+		return
+	}
+	book.Quantity -= 1
+	c.IndentedJSON(http.StatusOK, book)
+}
+
+func returnBook(c *gin.Context) {
+	id, ok := c.GetQuery("id")
+	if !ok {
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"message": "id parameter missing"})
+		return
+	}
+	book, err := bookById(id)
+	if err != nil {
+		c.IndentedJSON(http.StatusNotFound, gin.H{"message": err.Error()})
+		return
+	}
+	book.Quantity += 1
+	c.IndentedJSON(http.StatusOK, book)
+}
+
+func createBook(c *gin.Context) {
+	var newBook book
+	if err := c.BindJSON(&newBook); err != nil {
+		return
+	}
+	if b, _ := bookById(newBook.ID); b != nil {
+		c.IndentedJSON(http.StatusConflict, gin.H{"message": "book with id " + newBook.ID + " already exists"})
+		return
+	}
+	books = append(books, newBook)
+	c.IndentedJSON(http.StatusCreated, newBook)
+}
+
+func setRoutes(router *gin.Engine) {
+	router.GET("/books", getBooks)
+	router.GET("/books/:id", getBookByID)
+	router.POST("/books", createBook)
+	router.PATCH("/books/checkout", checkoutBook)
+	router.PATCH("/books/return", returnBook)
+}
